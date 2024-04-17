61ea287af9f7a4046aa26f6bc8f650580d40e36b
Mariana Pereira <marianapereira.s@live.com>
2024-04-17 17:14:26 -0300

    Criação da classe Inventory

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,17 @@
+from inventory_report.product import Product
+from typing import List, Optional
+
+
 class Inventory:
-    pass
+    def __init__(self, data: Optional[List[Product]] = None) -> None:
+        if data == None:
+            data = []
+
+        self._data = data
+
+    @property
+    def data(self):
+        return self._data.copy()
+
+    def add_data(self, data: List[Product]):
+        self._data.extend(data)
