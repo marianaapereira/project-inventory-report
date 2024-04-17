efabecc2f8dc5c54ba7115759fcdbcae9f84378f
Mariana Pereira <marianapereira.s@live.com>
2024-04-17 17:25:40 -0300

    Correções de lint no arquivo inventory_report/reports/report.py

--- inventory_report/reports/report.py
+++ inventory_report/reports/report.py
@@ -1,9 +1,10 @@
 from inventory_report.inventory import Inventory
 from typing import Protocol
 
+
 class Report(Protocol):
     def add_inventory(self, inventory: Inventory) -> None:
         ...
-    
+
     def generate(self) -> str:
         ...
