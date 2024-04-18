a993b3400c7d971484ebf605fbd3f52fddf5fa81
Mariana Pereira <marianapereira.s@live.com>
2024-04-17 22:53:21 -0300

    Criação do CompleteReport

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,19 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    def generate(self) -> str:
+        simple_report = super().generate()
+        companies_inventories = super().get_companies_inventories()
+        output_lines = []
+
+        for company in companies_inventories:
+            output_lines.append(f"- {company[0]}: {company[1]}\n")
+
+        output_lines = "".join(output_lines)
+
+        return (
+            f"{simple_report}"
+            "Stocked products by company:\n"
+            f"{output_lines}"
+        )
